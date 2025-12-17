891d340f9e25604ec812d65372c4243e977c237d
hellvita <seredaolga03@gmail.com>
2025-12-17 22:03:30 +0200

    remove the metadata from the NotFound component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,26 +1,4 @@
-import type { Metadata } from "next";
 import css from "./not-found.module.css";
-
-export const metadata: Metadata = {
-  title: "Page not found",
-  description: "Unfortunately, the page at this address does not exist!",
-  alternates: {
-    canonical: "/404",
-  },
-  openGraph: {
-    title: "Page not found",
-    description: "Unfortunately, the page at this address does not exist!",
-    url: "/404",
-    images: [
-      {
-        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
-        width: 1200,
-        height: 630,
-        alt: "NoteHub",
-      },
-    ],
-  },
-};
 
 export default function NotFound() {
   return (
