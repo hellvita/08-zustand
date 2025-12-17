a029107c90761415d8e5a9258fa92ac9440a8d44
hellvita <seredaolga03@gmail.com>
2025-12-17 21:36:18 +0200

    add the NotFountClient component

---
+++ app/NotFound.client.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import css from "./not-found.module.css";
+
+export default function NotFoundClient() {
+  const router = useRouter();
+  const handleBack = () => router.back();
+  return (
+    <div className={css.container}>
+      <h1 className={css.title}>404 - Page not found</h1>
+      <p className={css.description}>
+        Sorry, the page you are looking for does not exist.
+      </p>
+      <button onClick={handleBack} className={css.btn}>
+        Go back
+      </button>
+    </div>
+  );
+}
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,5 @@
-"use client";
-
 import type { Metadata } from "next";
-import { useRouter } from "next/navigation";
-import css from "./not-found.module.css";
+import NotFoundClient from "./NotFound.client";
 
 export const metadata: Metadata = {
   title: "Page not found",
@@ -23,17 +20,9 @@ export const metadata: Metadata = {
 };
 
 export default function NotFound() {
-  const router = useRouter();
-  const handleBack = () => router.back();
   return (
-    <div className={css.container}>
-      <h1 className={css.title}>404 - Page not found</h1>
-      <p className={css.description}>
-        Sorry, the page you are looking for does not exist.
-      </p>
-      <button onClick={handleBack} className={css.btn}>
-        Go back
-      </button>
-    </div>
+    <>
+      <NotFoundClient />
+    </>
   );
 }
