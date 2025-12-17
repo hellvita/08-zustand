40ad669711c3e3f71ac4aa2822dad32304bd88a2
hellvita <seredaolga03@gmail.com>
2025-12-17 18:57:51 +0200

    add the metadata to the not_found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,26 @@
 "use client";
 
+import type { Metadata } from "next";
 import { useRouter } from "next/navigation";
 import css from "./not-found.module.css";
+
+export const metadata: Metadata = {
+  title: "Page not found",
+  description: "Unfortunately, the page at this address does not exist!",
+  openGraph: {
+    title: "Page not found",
+    description: "Unfortunately, the page at this address does not exist!",
+    url: "coming soon",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "NoteHub",
+      },
+    ],
+  },
+};
 
 export default function NotFound() {
   const router = useRouter();
